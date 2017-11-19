f2ef46500d66b969da1f7241fd3217a527ead28e
Fab1as <32188663+Fab1as@users.noreply.github.com>
2017-11-20 00:14:57 +0300

    Add files via upload

---
+++ PS2/Part1/3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double e = double.Parse(Console.ReadLine());
+            int steps;
+            Console.WriteLine("Cosinus = {0:0.0000}", Math.Cos(x));
+            Console.WriteLine("Cos = {0:0.0000}", GetCos(x, e, out steps));
+            Console.WriteLine("Steps = " + steps);
+        }
+
+        public static double GetCos(double x, double e, out int k)
+        {
+            double temp = 1;
+            double cos = 0;
+            k = 1;
+            while (Math.Abs(temp) > e)
+            {
+                cos += temp;
+                temp *= -x * x / ((2 * k - 1) * (2 * k));
+                k++;
+            }
+            return cos;
+        }
+    }
+}
