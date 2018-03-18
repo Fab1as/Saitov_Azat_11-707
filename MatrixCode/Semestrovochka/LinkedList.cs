6f72f750267700ef1fc7566fb4b67ce0facff597
fabiasser <fabiasser@gmail.com>
2018-03-18 19:33:42 +0300

    Методы с описанием

--- MatrixCode/Semestrovochka/LinkedList.cs
+++ MatrixCode/Semestrovochka/LinkedList.cs
@@ -18,6 +18,12 @@ namespace Semestrovochka
         public Cell Current { get; set; }
         public int Count { get; private set; }
 
+        /// <summary>
+        /// Add element to the beginning of the list
+        /// </summary>
+        /// <param name="row">Line number of the cell</param>
+        /// <param name="column">Column number of the cell</param>
+        /// <param name="value">Value of the cell</param>
         public void AddFirst(int row, int column, int value)
         {
             var newCell = new Cell(){Row = row, Column = column, Value = value, Next = Head};
@@ -30,6 +36,12 @@ namespace Semestrovochka
             Count++;
         }
 
+        /// <summary>
+        /// Add element to the end of the list
+        /// </summary>
+        /// <param name="row">Line number of the cell</param>
+        /// <param name="column">Column number of the cell</param>
+        /// <param name="value">Value of the cell</param>
         public void AddLast(int row, int column, int value)
         {
             if (Head == null)
@@ -45,6 +57,10 @@ namespace Semestrovochka
             Count++;
         }
 
+        /// <summary>
+        /// Remove element from the list
+        /// </summary>
+        /// <param name="cell">Element to remove</param>
         public void Remove(Cell cell)
         {
             if (Head != null)
@@ -61,7 +77,11 @@ namespace Semestrovochka
             }
         }
 
-
+        /// <summary>
+        /// Remove element at specific position
+        /// </summary>
+        /// <param name="i">Row number of element</param>
+        /// <param name="j">Column number of element</param>
         public void Remove(int i, int j)
         {
             if (Head != null)
@@ -95,6 +115,9 @@ namespace Semestrovochka
             }
         }
 
+        /// <summary>
+        /// Remove element from beginning of the list
+        /// </summary>
         private void RemoveFromBegin()
         {
             if (Head == Current)
@@ -109,6 +132,10 @@ namespace Semestrovochka
             }
         }
 
+        /// <summary>
+        /// Remove element from the lis body
+        /// </summary>
+        /// <param name="cell">Element to remove</param>
         private void RemoveFromMiddle(Cell cell)
         {
             cell.Prev.Next = cell.Next;
@@ -117,6 +144,9 @@ namespace Semestrovochka
             cell.Next = null;
         }
 
+        /// <summary>
+        /// Remove element from the end of the list
+        /// </summary>
         private void RemoveFromEnd()
         {
             if (Head == Current)
@@ -131,6 +161,11 @@ namespace Semestrovochka
             }
         }
 
+        /// <summary>
+        /// Find element required element
+        /// </summary>
+        /// <param name="cell">Required element</param>
+        /// <returns>Required element</returns>
         public Cell FindCell(Cell cell)
         {
             if (cell == null)
@@ -147,13 +182,20 @@ namespace Semestrovochka
             return newCell;
         }
 
-
+        /// <summary>
+        /// Clear the list
+        /// </summary>
         public void Clear()
         {
             Current = Head = null;
             Count = 0;
         }
 
+        /// <summary>
+        /// Find element on certain position
+        /// </summary>
+        /// <param name="index">Index of the required element</param>
+        /// <returns>Required element on index position</returns>
         public Cell this[int index]
         {
             get
--- MatrixCode/Semestrovochka/MatrixCode.cs
+++ MatrixCode/Semestrovochka/MatrixCode.cs
@@ -5,9 +5,20 @@ namespace Semestrovochka
 {
     class MatrixCode
     {
+        /// <summary>
+        /// Cell storage list
+        /// </summary>
         public LinkedList ListMatrix;
+
+        /// <summary>
+        /// Dimension of the matrix
+        /// </summary>
         public const int Dimension = 10;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="matrix">Matrix with values</param>
         public MatrixCode(int[][] matrix)
         {
             if (matrix == null)
@@ -27,6 +38,10 @@ namespace Semestrovochka
             }
         }
 
+        /// <summary>
+        /// Converts the list to the matrix
+        /// </summary>
+        /// <returns>Matrix with values</returns>
         public int[][] Decode()
         {
             var array = new int[Dimension][];
@@ -45,6 +60,12 @@ namespace Semestrovochka
             return array;
         }
 
+        /// <summary>
+        /// Insert element to the list or replace it
+        /// </summary>
+        /// <param name="i">Element row</param>
+        /// <param name="j">Element column</param>
+        /// <param name="value">Element value</param>
         public void Insert(int i, int j, int value)
         {
             if (i >= Dimension || j >= Dimension || i < 0 || j < 0)
@@ -72,6 +93,11 @@ namespace Semestrovochka
             }
         }
 
+        /// <summary>
+        /// Delete element on certain row and column
+        /// </summary>
+        /// <param name="i">Element row</param>
+        /// <param name="j">Element column</param>
         public void Delete(int i, int j)
         {
             if (i >= Dimension || j >= Dimension || i < 0 || j < 0)
@@ -81,11 +107,19 @@ namespace Semestrovochka
             ListMatrix.Remove(i, j);
         }
 
+        /// <summary>
+        /// Delete required element
+        /// </summary>
+        /// <param name="cell">Required element to delete</param>
         public void Delete(Cell cell)
         {
             ListMatrix.Remove(cell);
         }
 
+        /// <summary>
+        /// Find minimal elements from each column
+        /// </summary>
+        /// <returns>ArrayList of minimal elements from each column</returns>
         public ArrayList MinList()
         {
             var valuesArray = new int[10];
@@ -125,6 +159,10 @@ namespace Semestrovochka
             return new ArrayList(valuesArray);
         }
 
+        /// <summary>
+        /// Calculates the sum of diagonals
+        /// </summary>
+        /// <returns>Sum of the elements on the diagonals</returns>
         public int SumDiag()
         {
             var cell = ListMatrix.Head;
@@ -142,6 +180,9 @@ namespace Semestrovochka
             return sum;
         }
 
+        /// <summary>
+        /// Transpose matrix
+        /// </summary>
         public void Transpose()
         {
             var cell = ListMatrix.Head;
@@ -153,6 +194,11 @@ namespace Semestrovochka
             }
         }
 
+        /// <summary>
+        /// Change elements in place
+        /// </summary>
+        /// <param name="a">First element</param>
+        /// <param name="b">Second element</param>
         private static void Swap(ref int a, ref int b)
         {
             var c = a;
@@ -160,6 +206,11 @@ namespace Semestrovochka
             b = c;
         }
 
+        /// <summary>
+        /// Summarize two columns and write result in first
+        /// </summary>
+        /// <param name="j1">First column number</param>
+        /// <param name="j2">Seconds column number</param>
         public void SumColumns(int j1, int j2)
         {
             if (j1 >= Dimension || j2 >= Dimension || j2 < 0 || j1 < 0)
